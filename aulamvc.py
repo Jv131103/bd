e48ca1954116c485e5a9913aeff7fd5b93f41b42
Jv131103 <120182670+Jv131103@users.noreply.github.com>
2023-05-08 20:55:21 -0300

    Add files via upload

--- aulamvc.py
+++ aulamvc.py
@@ -1,32 +1,32 @@
-from flask import Flask, render_template, request
 import os
+from flask import Flask, render_template, request
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
 app = Flask(__name__)
 
-app.config["MYSQL_DATABASE_USER"] = 'root'
-app.config["MYSQL_DATABASE_PASSWORD"] = 'x123'
-app.config["MYSQL_DATABASE_DB"] = 'teste'
-app.config["MYSQL_DATABASE_HOST"] = '172.17.0.2'
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'mudar123'
+app.config['MYSQL_DATABASE_DB'] = 'teste'
+app.config['MYSQL_DATABASE_HOST'] = '172.17.0.2'
 mysql.init_app(app)
 
 @app.route('/')
 def main():
-    return render_template("aulamvc.html")
+    return render_template('aulamvc.html')
 
-
-@app.route('/gravar', methods=["POST", "GET"])
+@app.route('/gravar', methods=['POST','GET'])
 def gravar():
-    nome = request.form["nome"]
-    email = request.form["email"]
-    senha = request.form["senha"]
-    if nome and email and senha:
-        conn = mysql.connect()
-        cursor = conn.cursor()
-        cursor.execute("insert into tbl_user(user_name, user_username, user_password) VALUES (%s, %s, %s)", (nome, email, senha))
-        conn.commit()
-    return render_template("aulamvc.html")
+  nome = request.form['nome']
+  email = request.form['email']
+  senha = request.form['senha']
+  if nome and email and senha:
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute('insert into tbl_user (user_name, user_username, user_password) VALUES (%s, %s, %s)', (nome, email, senha))
+    conn.commit()
+  return render_template('aulamvc.html')
 
 
 @app.route('/listar', methods=['POST','GET'])
@@ -38,6 +38,7 @@ def listar():
   conn.commit()
   return render_template('lista.html', datas=data)
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5008)
+    port = int(os.environ.get("PORT", 5008))
+    app.run(host='0.0.0.0', port=port)
+
